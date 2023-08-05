da7d5424f2532f0e0389c18f27ff25d95d0aa2f1
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2023-08-04 18:05:02 -0700

    Remove old information

--- src/Information.tsx
+++ src/Information.tsx
@@ -9,12 +9,6 @@ export function Informaton({ intervalSpeed }: Informaton) {
   return (
     <p style={{ position: 'fixed', right: 10, top: 0, color: 'black' }}>
       Moving layer every {intervalSpeed}ms
-      <br />
-      <br />
-      Open the console to see the keys warnings and look at the console log
-      <br />
-      <span style={{ fontWeight: 'bold' }}>below</span> the keys warning to see
-      liveblocks duplicating the array elements
     </p>
   );
 }
